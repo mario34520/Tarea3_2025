04614ca312d0f3fdd50af731ecbdddd888b37cf8
Mario Segura <marioale2385@gmail.com>
2025-02-09 16:47:32 -0600

    Tarea completada

--- app/src/main/java/com/tarea1/tarea3/MainActivity.kt
+++ app/src/main/java/com/tarea1/tarea3/MainActivity.kt
@@ -3,45 +3,19 @@ package com.tarea1.tarea3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.tarea1.tarea3.ui.theme.Tarea3Theme
+import androidx.compose.runtime.*
+import com.tarea1.tarea3.model.Estado
+import com.tarea1.tarea3.despliegue.componentes.Principal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Tarea3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            var estado by remember { mutableStateOf(Estado(contador = 0)) }
+
+            Principal(estado) { nuevoValor ->
+                estado = Estado(nuevoValor)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Tarea3Theme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/tarea1/tarea3/model/Estado.kt
@@ -0,0 +1,3 @@
+package com.tarea1.tarea3.model
+
+data class Estado(var contador: Int = 0)
